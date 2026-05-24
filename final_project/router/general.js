45529be283fd43ff934c2740c532edaa02356bf1
kynn237 <nematchouay@gmail.com>
2026-05-24 13:30:11 -0400

    Ajout route POST /register

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,19 +6,27 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: "Nom d'utilisateur et mot de passe requis" });
+    }
+
+    if (users.find(user => user.username === username)) {
+        return res.status(409).json({ message: "Utilisateur déjà existant" });
+    }
+
+    users.push({ username, password });
+    return res.status(201).json({ message: "Utilisateur créé avec succès" });
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
   const isbn = req.params.isbn;          // récupère l'ISBN depuis l'URL
   const book = books[isbn];              // books est l'objet importé plus haut
 
@@ -51,7 +59,6 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
   const titleName = req.params.title.toLowerCase();
   const result = [];
   for (let isbn in books) {
